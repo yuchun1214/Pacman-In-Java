0057d47e3cf955ce9e476bc2901f46e5ae598d1f
Rocky <enterniter@gmail.com>
2020-06-03 20:39:36 +0800

    try

--- src/pacMan/Condition.java
+++ src/pacMan/Condition.java
@@ -1,26 +1,32 @@
-//package pacMan;
-//
-//import java.awt.Graphics;
-//import java.awt.Image;
-//import java.io.File;
-//import java.io.IOException;
-//
-//import javax.imageio.ImageIO;
-//import javax.swing.JComponent;
-//
-//public abstract class Condition extends JComponent {
-//	public Image loseIconImage;
-//	public Image winIconImage;
-//	public Image heart1;
-//	public Image heart2;
-//	public Image heart3;
-//	
-//	public void loadImage() {
-////		this.loseIconImage = ImageIO.read(new File("src/image/win_fail/fail.png"));
-////		this.winIconImage = ImageIO.read(new File("src/image/win_fail/win.png"));
-////		this.heart1 = ImageIO.read(new File("src/image/win_fail/heart1.png"));
-////		this.heart2 = ImageIO.read(new File("src/image/win_fail/heart2.png"));
-////		this.heart3 = ImageIO.read(new File("src/image/win_fail/heart3.png"));
-//	}
-//	
-//}
+package pacMan;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+public abstract class Condition extends JComponent {
+	Image loseIconImage;
+	Image winIconImage;
+	Image heart1;
+	Image heart2;
+	Image heart3;
+	
+	public void loadImage() throws IOException {
+		this.loseIconImage = ImageIO.read(new File("src/image/win_fail/fail.png"));
+		this.winIconImage = ImageIO.read(new File("src/image/win_fail/win.png"));
+		this.heart1 = ImageIO.read(new File("src/image/win_fail/heart1.png"));
+		this.heart2 = ImageIO.read(new File("src/image/win_fail/heart2.png"));
+		this.heart3 = ImageIO.read(new File("src/image/win_fail/heart3.png"));
+	}
+	public void paint(Graphics g) {
+		paintLose(g);
+	}
+	public void paintLose(Graphics g) {
+		g.drawImage(loseIconImage,10,10,  500,  500,  null);
+	}
+	
+}
