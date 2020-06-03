eaf840c49c8541fb6310f06a114eae641afd849b
Rocky <enterniter@gmail.com>
2020-06-03 20:25:12 +0800

    try

---
+++ src/pacMan/Condition.java
@@ -0,0 +1,26 @@
+//package pacMan;
+//
+//import java.awt.Graphics;
+//import java.awt.Image;
+//import java.io.File;
+//import java.io.IOException;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.JComponent;
+//
+//public abstract class Condition extends JComponent {
+//	public Image loseIconImage;
+//	public Image winIconImage;
+//	public Image heart1;
+//	public Image heart2;
+//	public Image heart3;
+//	
+//	public void loadImage() {
+////		this.loseIconImage = ImageIO.read(new File("src/image/win_fail/fail.png"));
+////		this.winIconImage = ImageIO.read(new File("src/image/win_fail/win.png"));
+////		this.heart1 = ImageIO.read(new File("src/image/win_fail/heart1.png"));
+////		this.heart2 = ImageIO.read(new File("src/image/win_fail/heart2.png"));
+////		this.heart3 = ImageIO.read(new File("src/image/win_fail/heart3.png"));
+//	}
+//	
+//}
